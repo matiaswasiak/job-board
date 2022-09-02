3a3f692e603424ae92dcc63afea3060befeb2bd3
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-01 23:36:09 -0300

    Company-Jobs Association

---
+++ client/src/components/CompanyDetail.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getCompany } from "../graphql/queries";
+import JobList from "./JobList";
+
+function CompanyDetail() {
+  const [company, setcompany] = useState(null);
+  const { companyId } = useParams();
+
+  useEffect(() => {
+    getCompany(companyId).then(setcompany);
+  }, [companyId]);
+
+  if (!company) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <h1 className="title">{company.name}</h1>
+      <div className="box">{company.description}</div>
+      <h5 className="title is-5">Jobs at {company.name}</h5>
+      <JobList jobs={company.jobs} />
+    </div>
+  );
+}
+
+export default CompanyDetail;
